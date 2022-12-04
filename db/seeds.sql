c437b8613764fa56b27b800a5edcd59eeaec858b
RAHMA_BILAL <rahma.bilal15@gmail.com>
2022-12-04 20:37:02 +0000

    adding information for each table

--- db/seeds.sql
+++ db/seeds.sql
@@ -1,33 +1,40 @@
 
 INSERT INTO department (name)
 VALUES 
-        ("IT"),
-        ("Customer Service"),
-        ("Retail"),
-        ("Human Resources");
-
-SELECT * FROM department;
+        ("Key Creative Team"),
+        ("Production"),
+        ("Script"),
+        ("Location"),
+        ("Camera"),
+        ("Sound"),
+        ("Hair & Makeup"),
+        ("Wardrobe"),
+        ("Media");
+-- SELECT * FROM department;
 
 INSERT INTO role (title, salary, department_id)
 VALUES
-        ("Manager", 135000, 1),
-        ("HR Officer", 22000, 4),
+        ("Producer", 135000, 1),
+        ("Director", 22000, 4),
+        ("Screenwriter", 32000, 3),
+        ("Script Supervisor", 21000, 4),
+        ("Stunt Coordinator", 19000, 2),
+        ("Script Editor", 22000, 3),
+        ("Camera Operator", 25000, 2),
+        ("Costume Designer", 23000, 4),
         ("Intern", 20000, 1);
-        ("Data Analyst", 32000, 3),
-        ("Retail Assitant", 21000, 4),
-        ("Customer Service Assistant", 19000, 2)
 
 
-SELECT * FROM role;
+-- SELECT * FROM role;
 
 INSERT INTO employee (first_name, last_name, role_id, manager_id)
 VALUES 
-        ("Harry", "Potter", 1, 1),
+        ("Harry", "Potter", 1, NULL),
         ("Hermione", "Granger", 2, NULL),
-        ("Ron", "Weasley", 3, 3),
-        ("Draco", "Malfoy", 4, 5),
-        ("Rubeus", "Hagrid", 5, NULL),
-        ("Lord", "Voldemort", 6, 3),
-        ("Sirius", "Black", 7, NULL);
+        ("Ron", "Weasley", 3, 1),
+        ("Draco", "Malfoy", 4, 1),
+        ("Rubeus", "Hagrid", 5, 1),
+        ("Lord", "Voldemort", 6, 1),
+        ("Sirius", "Black", 2, NULL);
 
-SELECT * FROM employee;
+-- SELECT * FROM employee;
